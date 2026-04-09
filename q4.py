4a9eb7afbe795f7ba979c99c3bc7f1fc7b98ecb3
naitikraj3101 <smkr809@gmail.com>
2026-04-09 22:50:43 +0530

    add initial implementation of number positivity check program

---
+++ q4.py
@@ -0,0 +1,10 @@
+# Q program to check if a number is positive or negative or 0
+
+x = int(input('enter your number : '))
+
+if x > 0:
+    print('positive number')
+elif x == 0:
+    print('number is 0')
+else:
+    print('number is negative')
